db0264bc7830e34b0b2d6c58294cc4fc1c907c71
Nataly1077 <137103067+Nataly1077@users.noreply.github.com>
2023-06-26 16:23:14 +0400

    Add files via upload

---
+++ DZ 11.1/DZ_11.1.py
@@ -0,0 +1,12 @@
+﻿def factorial(n):
+    from math import factorial
+    s = []
+    num = factorial(n)
+    for i in range(num, 0, -1):
+        a = factorial(i)
+        s.append(a)
+    print('Список факториалов от факториала числа', n, ':', s)
+
+print('Введите число')
+n = int(input())
+factorial(n)
