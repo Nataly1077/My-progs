f1ca70b01c501d62651125a1e2548afd9f173c46
Nataly1077 <137103067+Nataly1077@users.noreply.github.com>
2023-06-27 16:26:12 +0400

    Add files via upload

---
+++ DZ 13/DZ_13.py
@@ -0,0 +1,18 @@
+﻿from random import randint
+print('Введите количество строк матрицы:')
+n = int(input())
+print('Введите количество столбцов матрицы:')
+m = int(input())
+
+matrix_1 = [[randint(1, 10) for j in range(m)] for i in range(n)]
+matrix_2 = [[randint(1, 10) for j in range(m)] for i in range(n)]
+
+matrix_3 = [[matrix_1[i][j] + matrix_2[i][j]  for j in range
+(len(matrix_1[0]))] for i in range(len(matrix_1))]
+ 
+print('Матрица # 1:')
+print(matrix_1)
+print('Матрица # 2:')
+print(matrix_2)
+print('Результат сложения матриц:')
+print(matrix_3)
