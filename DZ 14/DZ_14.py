61cf0f4a15f0d0244882d67f94b4970c422ff16d
Nataly1077 <137103067+Nataly1077@users.noreply.github.com>
2023-06-27 17:22:16 +0400

    Add files via upload

---
+++ DZ 14/DZ_14.py
@@ -0,0 +1,10 @@
+﻿my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+ 
+def rec(my_list):
+    if my_list == []:
+        print("Конец списка.")
+    else:
+        print(my_list.pop(0))
+        rec(my_list)
+    
+rec(my_list)
