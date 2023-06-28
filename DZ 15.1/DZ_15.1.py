73e887fe719b54d15fea9e6d3ff546e77db0cf7c
Nataly1077 <137103067+Nataly1077@users.noreply.github.com>
2023-06-28 18:12:43 +0400

    Add files via upload

---
+++ DZ 15.1/DZ_15.1.py
@@ -0,0 +1,17 @@
+﻿class Transport:
+    
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+        
+class Autobus(Transport):
+    
+    def __init__(self, name, max_speed, mileage):
+        super().__init__(name, max_speed, mileage)
+        
+    def __str__(self):
+        return "Марка: "+self.name+" Макс. скорость: "+str(self.max_speed)+" Пробег: "+str(self.mileage)
+        
+bus=Autobus("Renault Logan", 180, 12)
+print(bus)
